c892a435af64d09966654b5fffaef1559cd36483
oskareek <oskar.eek99@outlook.com>
2023-03-01 22:56:06 +0100

    Recipe generation

---
+++ foodGenerator/src/lib/MiniRecipe.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let recipe = {}
+    let dropDownActive = false;
+
+    function toggle() {
+        dropDownActive = !dropDownActive;
+    }
+    $: defaultHidden = !dropDownActive ? "none" : "";
+    $: defaultVisable = dropDownActive ? "none" : "";
+</script>
+
+<div class="bg-dark text-white rounded-end">
+    <h3>{recipe.name}</h3>
+    <button style="display: {defaultVisable}" on:click={toggle}>Öppna</button>
+    <div style="display: {defaultHidden}">
+        <p>{recipe.description}</p>
+        <ul>
+            {#each recipe.products as product}
+                <li>
+                    <p>{product.name + " " + product.amount + product.measurement}</p>
+                </li>
+            {/each}
+        </ul>
+        <button style="display: {defaultHidden}" on:click={toggle}>Stäng</button>
+    </div>
+</div>
--- foodGenerator/src/routes/recipes/+page.svelte
+++ foodGenerator/src/routes/recipes/+page.svelte
@@ -1,9 +1,41 @@
 <script>
-    import Recipe from '$lib/Recipe.svelte'
+    import MiniRecipe from '$lib/MiniRecipe.svelte'
 
+    let numberInput;
+    let generatedRecipes = []
+
+    function generateRecipes() {
+        if (numberInput > 10) return
+
+        //Reset array
+        generatedRecipes = [];
+
+        //Add number of requested recipes to the "generatedRecipes" array
+        for (let i = 0; i < numberInput; i++) {
+            let randomIndex = Math.floor(Math.random() * (storedRecipes.length))
+            let recipe = storedRecipes[randomIndex]
+            
+            //No need to try to get unique recipe if all possible recipes already in list
+            if (generatedRecipes.length < storedRecipes.length) {
+                //Get a new recipe until we get one thats not already in the list
+                while (objectExistsInList(generatedRecipes, "id", recipe.id)) {
+                    randomIndex = Math.floor(Math.random() * (storedRecipes.length))
+                    recipe = storedRecipes[randomIndex]
+                }
+            }
+
+            generatedRecipes = [recipe, ...generatedRecipes]
+        }
+    }
+
+    function objectExistsInList(array, objectProperty, value){
+        return !!array.filter(function(item){
+            return item[objectProperty] == value;
+        }).length;
+    }
     
-    let recipes = [
-        {id: 1, name: "Körtfärsås", description: "Koka spaghetti, stek köttfärs", products: [
+    let storedRecipes = [
+        {id: 1, name: "Körtfärsås och spaghetti", description: "Koka spaghetti, stek köttfärs", products: [
             {name: "köttfärs", amount: 500, measurement: "grams"},
             {name: "spaghetti", amount: 500, measuremnt: "grams"},
             {name: "tomatsås", amount: 100, measurement: "millilitres"},
@@ -18,11 +50,39 @@
             {name: "permesan-ost", amount: 60, measurement: "grams"}
             ],
             numberOfPeople: 3
+        },
+        {id: 3, name: "Kycklingrullar", description: "Koka spaghetti, stek bacon, hacka lök", products: [
+            {name: "Bacon", amount: 200, measurement: "grams"},
+            {name: "spaghetti", amount: 600, measurement: "grams"},
+            {name: "lök", amount: 1, measurement: "st"},
+            {name: "permesan-ost", amount: 60, measurement: "grams"}
+            ],
+            numberOfPeople: 3
+        },
+        {id: 4, name: "Tacos", description: "Koka spaghetti, stek bacon, hacka lök", products: [
+            {name: "Bacon", amount: 200, measurement: "grams"},
+            {name: "spaghetti", amount: 600, measurement: "grams"},
+            {name: "lök", amount: 1, measurement: "st"},
+            {name: "permesan-ost", amount: 60, measurement: "grams"}
+            ],
+            numberOfPeople: 3
+        },
+        {id: 5, name: "Falukorv och Makaroner", description: "Koka spaghetti, stek bacon, hacka lök", products: [
+            {name: "Bacon", amount: 200, measurement: "grams"},
+            {name: "spaghetti", amount: 600, measurement: "grams"},
+            {name: "lök", amount: 1, measurement: "st"},
+            {name: "permesan-ost", amount: 60, measurement: "grams"}
+            ],
+            numberOfPeople: 3
         }
     ]
 
 </script>
 
-{#each recipes as recipe}
-	<Recipe recipe={recipe} />
+<label>Antal maträtter som ska genereras:</label>
+<input type="text" bind:value={numberInput} />
+<button on:click={generateRecipes}>Generate</button>
+{#each generatedRecipes as recipe, i}
+    <h2>Dag {i + 1}</h2>
+	<MiniRecipe recipe={recipe} />
 {/each}
